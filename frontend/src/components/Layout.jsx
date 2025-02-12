ece5671e96a09b50c7f96b8858c9e55c9676204e
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-12 15:33:52 +0530

    first commite

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Routers from "../router/Routers";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Routers />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
