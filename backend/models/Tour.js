43b867bda997265126eec68d09045944e6c32fc2
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-19 17:51:20 +0530

    change tour model

--- backend/models/Tour.js
+++ backend/models/Tour.js
@@ -6,32 +6,17 @@ const tourSchema = new mongoose.Schema(
     type: { type: String, enum: ["round", "day"], required: true },
     category: { type: [String], required: true },
     description: { type: String, required: true },
+    overview: { type: String, required: true },
     image: { type: String, required: true },
     duration: { type: String, required: true },
-    maxPeople: { type: Number, required: true },
     price: { type: Number, required: true },
-    currency: { type: String, default: "USD" },
     locations: { type: [String], required: true },
     destinations: { type: [String], required: true },
     
     isOfferAvailable: { type: Boolean, default: false },
     offerPercentage: { type: Number, default: 0 },
     offerPrice: { type: Number, default: null },
-
-    ratings: {
-      avgRating: { type: Number, default: 0 },
-      totalReviews: { type: Number, default: 0 }
-    },
-
-    reviews: [
-      {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        userName: { type: String, required: true },
-        rating: { type: Number, required: true },
-        comment: { type: String, required: true },
-        date: { type: Date, default: Date.now }
-      }
-    ],
+    isPopular: { type: Boolean, default: false },
 
     highlights: { type: [String], required: true },
     includes: { type: [String], required: true },
@@ -43,15 +28,12 @@ const tourSchema = new mongoose.Schema(
         route: { type: String, required: true },
         title: { type: String, required: true },
         image: { type: String },
-        activities: { type: [String], required: true }
+        activities: { type: [String], required: true },
       }
     ],
 
-    customizable: { type: Boolean, default: false },
-    additionalServices: { type: [String], default: [] },
-    relatedTours: { type: [mongoose.Schema.Types.ObjectId], ref: "Tour" },
-
-    status: { type: String, enum: ["active", "inactive"], default: "active" },
+  mapImage: { type: String, required: true },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
   },
   { timestamps: true }
 );
